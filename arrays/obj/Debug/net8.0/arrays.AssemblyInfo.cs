a46b0838b168f14ead8ad26c1ad07c55da23c6bf
ERP-Popoler <153335646+ERP-Popoler@users.noreply.github.com>
2024-06-28 23:10:00 -0400

    TERMINADO

--- arrays/obj/Debug/net8.0/arrays.AssemblyInfo.cs
+++ arrays/obj/Debug/net8.0/arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+513b28272c76fad75d5d3f514f419e7acad873b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada03e52e51046a09feb9f45d5d5fffe5ee45373")]
 [assembly: System.Reflection.AssemblyProductAttribute("arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
